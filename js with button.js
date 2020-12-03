311032a5a2e21d95e8e27a4287c32d70bc2c355b
stockmeta <altimbs@gmail.com>
2020-12-03 18:54:33 +0800

    nothing much to change

---
+++ js with button.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Javascript</h2>
+
+<p id="js"></js>
+
+<script>
+<button type="button" onclick='document.getElementById("js").innerHTML="Hello Javascript!"'>Click Me!</button>
+</script>
+
+</body>
+</html>
---
+++ practice.js
@@ -0,0 +1,20 @@
+// var age = 22
+
+// if (age >= 19) {
+//  console.log("I am 22 years old");
+// }
+//  else if (age <= 23) {
+//    console.log("I am not 22 years old");
+//  }
+
+// var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (var number=1; number<=4; number++) {
+//  console.log(number);
+// }
+
+//var names = ["Al", "Jeffrey", "Mae", "Philip", "Ann"];
+
+//for (var y=0; y<=names.length; y++) {
+  //console.log(names[y]);
+//}
